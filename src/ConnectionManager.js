7a40ebf4b42417f07ea18a332220b38b2801897f
Julian Gieseke <jgieseke@mesosphere.com>
2018-02-03 11:17:07 +0100

    thesis bulk commit

--- src/ConnectionManager.js
+++ src/ConnectionManager.js
@@ -1,33 +1,15 @@
 import { List } from "immutable";
 import { AbstractConnection, ConnectionEvent } from "@dcos/connections";
 import ConnectionQueueItem from "./ConnectionQueueItem";
-
-const PRIORITY_STUDENT = 1;
-const PRIORITY_BATCH = 2;
-const PRIORITY_INTERACTIVE = 3;
-const PRIORITY_SYSTEM = 4;
+import TokenStore from "./TokenStore";
 
 export default class ConnectionManager {
-
-  static get PRIORITY_STUDENT() {
-    return PRIORITY_STUDENT;
-  }
-  static get PRIORITY_BATCH() {
-    return PRIORITY_BATCH;
-  }
-  static get PRIORITY_INTERACTIVE() {
-    return PRIORITY_INTERACTIVE;
-  }
-  static get PRIORITY_SYSTEM() {
-    return PRIORITY_SYSTEM;
-  }
-
   /**
    * Initializes an Instance of ConnectionManager
    *
    * @param {int} maxConnections – max open connections
    */
-  constructor(maxConnections = 5, maxAutoPriority = PRIORITY_INTERACTIVE) {
+  constructor(maxConnections = 6, maxPriority = 4) {
     /**
      * Private Context
      *
@@ -56,26 +38,19 @@ export default class ConnectionManager {
       interval: null,
 
       /**
-       * @property {ConnectionQueue} paused
-       * @description paused
-       * @name ConnectionManager~Context#paused
+       * @property {ConnectionQueue} tokens
+       * @description internal token store
+       * @name ConnectionManager~Context#tokens
        */
-      paused: false,
-
-      /**
-       * @property {ConnectionQueue} token
-       * @description token
-       * @name ConnectionManager~Context#token
-       */
-      token: null,
+      tokens: new TokenStore(),
 
       /**
        * Opens a Connection
-       * 
+       *
        * @param {AbstractConnection} connection
        */
       setupConnection(connection) {
-        connection.open({Authentication: `Bearer ${this.token}`});
+        connection.open(this.tokens.getHeader(connection));
       },
 
       /**
@@ -83,36 +58,25 @@ export default class ConnectionManager {
        */
       loop() {
         // store all open items
-        let openList = context.list.filter(listItem => 
-          listItem.connection.state === AbstractConnection.OPEN
+        let openList = context.list.filter(
+          listItem => listItem.connection.state === AbstractConnection.OPEN
         );
-        
+
         let waitingList = context.list
-          .filter(listItem => 
-            listItem.connection.state === AbstractConnection.INIT
+          .filter(
+            listItem => listItem.connection.state === AbstractConnection.INIT
           )
-          // this code would increase priority on old items, after they lived in the line for more then 30s.
-          // It is possible better to do this for every connection itself instead of doing it globally
-          // here, porbably not all connections need to increase their priority.
-          // .map((listItem) => {
-          //   if (listItem.priority >= maxAutoPriority) {
-          //     return listItem;
-          //   }
-
-          //   if(listItem.created < Date.now() - 30000) {
-          //     return new ConnectionQueueItem(listItem.connection, listItem.priority+1);
-          //   }
-
-          //   return listItem;
-          // })
-          // and sort
           .sortBy(listItem => -1 * listItem.priority);
-        
+
         // if there are free slots, start as much tasks as possible
         // this has to be a while because otherwise only one connection
         // per second would be opened when the tab is inactive.
         // see https://stackoverflow.com/questions/15871942/how-do-browsers-pause-change-javascript-when-tab-or-window-is-not-active
-        while(waitingList.size && openList.size < maxConnections) {
+        while (
+          waitingList.size &&
+          maxPriority - waitingList.first().priority <
+            maxConnections - openList.size
+        ) {
           const waitingItem = waitingList.first();
           waitingList = waitingList.shift();
           context.setupConnection(waitingItem.connection);
@@ -122,28 +86,24 @@ export default class ConnectionManager {
         // merge the lists again (this removes closed connections).
         context.list = openList.concat(waitingList);
 
-        // still waiting items? 
-        if(!context.paused && waitingList.size > 0) {
-          const delay = 250;
-          context.interval = setTimeout(
-            context.loop, 
-            delay
-          );
+        // still waiting items?
+        if (waitingList.size > 0) {
+          // https://www.immagic.com/eLibrary/ARCHIVES/GENERAL/NNG_US/N930101N.pdf
+          context.interval = setTimeout(context.loop, 100);
         } else {
           context.interval = null;
         }
       },
 
       /**
-     * schedules connection in loop
-     *
-     * @this ConnectionManager~Context
-     * @param {AbstractConnection} connection – connection to queue
-     * @param {Integer} [priority] – optional change of priority
-     * @return {AbstractConnection} - the scheduled connection
-     */
+       * schedules connection in loop
+       *
+       * @this ConnectionManager~Context
+       * @param {AbstractConnection} connection – connection to queue
+       * @param {Integer} [priority] – optional change of priority
+       * @return {AbstractConnection} - the scheduled connection
+       */
       schedule(connection, priority) {
-
         // create a new QueueItem to have the correct default priority
         const item = new ConnectionQueueItem(connection, priority);
 
@@ -151,36 +111,29 @@ export default class ConnectionManager {
         if (connection.state === AbstractConnection.CLOSED) {
           return;
         }
-    
+
         // add, sort and process
         context.list = context.list
           .filter(listItem => !listItem.equals(item))
           .push(item);
 
         // running loop? otherwise start it
-        if(!context.paused && !context.interval) {
-          context.interval = setTimeout(
-            context.loop, 
-            0
-          );
+        if (!context.interval) {
+          context.interval = setTimeout(context.loop, 0);
         }
 
+        // return the given connection for further use
         return connection;
       }
     };
 
     this.schedule = this.schedule.bind(context);
-    
-    this.setToken = this.setToken.bind(context);
-
-    this.pause = this.pause.bind(context);
-    this.resume = this.resume.bind(context);
-
+    this.storeToken = this.storeToken.bind(context);
     this.list = this.list.bind(context);
   }
 
   /**
-   * Calls internal schedule method
+   * This one only calls the "internal" schedule method
    *
    * @this ConnectionManager~Context
    * @param {AbstractConnection} connection – connection to queue
@@ -191,52 +144,12 @@ export default class ConnectionManager {
     return this.schedule(connection, priority);
   }
 
-  /**
-   * pauses the loop processing
-   *
-   * @this ConnectionManager~Context
-   */
-  pause() { 
-    this.paused = true;
-    if(this.interval) {
-      window.clearTimeout(this.interval);
-      this.interval = null
-    }
+  storeToken(token) {
+    console.log(`ConnectionManager~storeToken: Storing the Token.`);
+    this.tokens.store(token);
   }
 
-  /**
-   * resumes the loop processing
-   *
-   * @this ConnectionManager~Context
-   */
-  resume() { 
-    this.paused = false;
-    if(!this.paused && !this.interval && this.list.filter(listItem => 
-        listItem.connection.state === AbstractConnection.INIT
-    )) {
-      this.interval = setTimeout(
-        this.loop, 
-        0
-      );
-    }
-  }
-
-  /**
-   * Updates stored token to authenticate requests
-   * 
-   * @param {string} token - valid token
-   */
-  setToken(token) {
-    this.token = token;
-  }
-
-  /**
-   * debugging method, returns list
-   *
-   * @this ConnectionManager~Context
-   * @return {List} - connectionList
-   */
-  list() { 
+  list() {
     return this.list;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { default as CM } from "./ConnectionManager";
 export { default as ConnectionQueueItem } from "./ConnectionQueueItem";
 
+// this will be done through proper dependency injection, 
+// but for this prototype, it should be good enough
 export default new CM();
 
 export const ConnectionManager = CM;
