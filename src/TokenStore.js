ed98f1286483f2ec9d381aff48e9255a43e41b48
Julian Gieseke <jgieseke@mesosphere.com>
2018-02-03 11:15:35 +0100

    feat(TokenStore): ointroduce TokenStore

---
+++ src/TokenStore.js
@@ -0,0 +1,23 @@
+import AbstractConnection from "@dcos/connections";
+
+export default class TokenStore {
+  constructor() {
+    const context = {
+      instance: this,
+      token: null
+    };
+
+    this.store = this.store.bind(context);
+    this.get = this.get.bind(context);
+  }
+
+  store(token = null) {
+    this.token = token;
+    console.log(`TokenStore~store: Token stored.`);
+  }
+
+  getHeader(connection) {
+    // in future this method will get the correct token (or none) for the given url
+    return { "Authentication": `Bearer: ${this.token}`};
+  }
+}
